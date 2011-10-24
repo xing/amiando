8d1aa4a859e3c64b1777a6528d3cdcd13d935e71
Jorge Dias <jorge@mrdias.com>
2011-10-24 13:04:35 +0200

    Support for factories created on request

--- test/support/factory.rb
+++ test/support/factory.rb
@@ -1,15 +1,29 @@
 module Amiando
   def self.Factory(name, options = {})
-    case name
-    when :api_key
-      Amiando::ApiKey.create({:name => 'wadus'}.merge(options))
-    when :user
-      Amiando::User.create({
-        :first_name => 'Jorge',
-        :last_name  => 'Llop',
-        :password   => '123456',
-        :language   => 'es'
-      }.merge(options))
+    Factory.build(name, options)
+  end
+
+  module Factory
+    extend self
+
+    def build(name, options)
+      case name
+      when :api_key
+        Amiando::ApiKey.create({:name => 'wadus'}.merge(options))
+      when :user
+        Amiando::User.create({
+          :first_name => 'Jorge',
+          :last_name  => 'Llop',
+          :password   => '123456',
+          :language   => 'es'
+        }.merge(options))
+      end
+    end
+
+    def create(name, options)
+      object = build(name, options)
+      Amiando.run
+      object
     end
   end
 end
